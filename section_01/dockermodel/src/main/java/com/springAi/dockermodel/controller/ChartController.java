8e7e4bc3151a9afc244210d9a6635d73cc87817e
Ashok Gunji <21ne5a0225@gmail.com>
2025-08-24 21:28:01 +0530

    spring ai

---
+++ section_01/dockermodel/src/main/java/com/springAi/dockermodel/controller/ChartController.java
@@ -0,0 +1,23 @@
+package com.springAi.dockermodel.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChartController {
+
+    private final ChatClient chatClient;
+
+    public ChartController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @RequestMapping("/chat")
+    public String getChart(@RequestParam("message") String message) {
+
+        return  chatClient.prompt(message).call().content();
+    }
+}
