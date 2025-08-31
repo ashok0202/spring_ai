2db427ce60f0e78157dd249695da70d44323a01f
Ashok Gunji <21ne5a0225@gmail.com>
2025-08-31 13:12:20 +0530

    added multi models

---
+++ section_01/awsBedrock/src/test/java/com/springai/awsBedrock/AwsBedrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.awsBedrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBedrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
