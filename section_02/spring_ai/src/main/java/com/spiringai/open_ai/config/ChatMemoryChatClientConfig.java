3ae15a29c7cee91c1994043cdc0394bfda199ab7
Ashok Gunji <21ne5a0225@gmail.com>
2025-10-12 23:23:08 +0530

    implemented chat memory functionlity

---
+++ section_02/spring_ai/src/main/java/com/spiringai/open_ai/config/ChatMemoryChatClientConfig.java
@@ -0,0 +1,26 @@
+package com.spiringai.open_ai.config;
+
+import com.spiringai.open_ai.advisors.TokenUsageAuditAdvisor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatMemoryChatClientConfig {
+
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        Advisor advisor = new SimpleLoggerAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return chatClientBuilder
+                .defaultAdvisors(List.of(advisor, memoryAdvisor))
+                .build();
+    }
+}
