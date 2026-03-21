de0878e92af3cf45ddd982869eb9af6d14c4571d
Ashok Gunji <21ne5a0225@gmail.com>
2026-03-21 17:04:05 +0530

    added Mcp elicitataion in mcp server side

---
+++ section_07/mcpelicitation/src/main/java/com/mcpserver/elicitation/McpelicitationApplication.java
@@ -0,0 +1,13 @@
+package com.mcpserver.elicitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpelicitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpelicitationApplication.class, args);
+	}
+
+}
---
+++ section_07/mcpelicitation/src/test/java/com/mcpserver/elicitation/McpelicitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcpserver.elicitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpelicitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
